d014b743cc37f20feacf1687993446d4fbf15f75
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-12-20 00:30:25 +0900

    POSTされたURLからフレーム画像を取得する処理を追加

--- src/api.py
+++ src/api.py
@@ -1,19 +1,38 @@
 import os
+import datetime
 import pafy
+import cv2
 from fastapi import FastAPI
 import subprocess
 from subprocess import PIPE
 from pydantic import BaseModel
 
+app = FastAPI()
+
 class GreetingModel(BaseModel):
+    detect_url: str
     detect_mode: int
     detect_class: int
 
-app = FastAPI()
-
 @app.post("/detect/")
-async def detect(int: GreetingModel):
-    proc = subprocess.run('python yolov5/detect.py', stdout=PIPE, stderr=PIPE, shell=True)
-    proc_str = eval(proc.stdout.decode('utf-8'))
+async def detect(text:GreetingModel):
+    dir_path = 'target_images'
+    basename = 'camera_capture_cycle'
+    ext = 'jpg'
 
-    return proc_str
+    video = pafy.new(text.detect_url)
+    best = video.getbest(preftype="mp4")
+    cap = cv2.VideoCapture(best.url)
+    
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
+
+    ret, frame = cap.read()
+    img_path = cv2.imwrite('{}_{}.{}'.format(base_path, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'), ext), frame)
+
+    '''
+        proc = subprocess.run('python yolov5/detect.py', stdout=PIPE, stderr=PIPE, shell=True)
+        proc_str = eval(proc.stdout.decode('utf-8'))
+
+    '''
+    return text.detect_url
