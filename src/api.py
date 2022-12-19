2c8b7877b366c5a9d49d10c3625cbb83400bdd40
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-12-20 00:59:36 +0900

    POSTされたURLから得られた推論結果を返す処理を追加

--- src/api.py
+++ src/api.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import datetime
 import pafy
 import cv2
@@ -10,29 +11,28 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class GreetingModel(BaseModel):
-    detect_url: str
+    detect_id: int
     detect_mode: int
-    detect_class: int
+    detect_url: str
 
 @app.post("/detect/")
 async def detect(text:GreetingModel):
-    dir_path = 'target_images'
+    dir_path = 'target_images/%s' % text.detect_id
     basename = 'camera_capture_cycle'
     ext = 'jpg'
+    
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
 
     video = pafy.new(text.detect_url)
     best = video.getbest(preftype="mp4")
     cap = cv2.VideoCapture(best.url)
-    
     os.makedirs(dir_path, exist_ok=True)
     base_path = os.path.join(dir_path, basename)
-
     ret, frame = cap.read()
-    img_path = cv2.imwrite('{}_{}.{}'.format(base_path, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'), ext), frame)
+    cv2.imwrite('{}_{}.{}'.format(base_path, datetime.datetime.now().strftime('%Y%m%d%H%M%S%f'), ext), frame)
 
-    '''
-        proc = subprocess.run('python yolov5/detect.py', stdout=PIPE, stderr=PIPE, shell=True)
-        proc_str = eval(proc.stdout.decode('utf-8'))
+    proc = subprocess.run('python yolov5/detect.py --source %s' % dir_path, stdout=PIPE, stderr=PIPE, shell=True)
+    proc_str = eval(proc.stdout.decode('utf-8'))
 
-    '''
-    return text.detect_url
+    return proc_str 
